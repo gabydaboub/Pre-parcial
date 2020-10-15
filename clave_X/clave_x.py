daa37d9455b87745e08321321b008c2ff572cac8
Gabriela daboub <gabydaboub@gmail.com>
2020-10-14 22:42:32 -0600

    Pre Parcial

---
+++ clave_X/clave_x.py
@@ -0,0 +1,56 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma():
+    suma = 2+4
+    return suma
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion():
+    multip = 2*4*5
+    return multip
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista():
+    numerosLista = [2,5,4,6,9,12]
+    return sum(numerosLista)
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return "https://github.com/gabydaboub/Pre-parcial.git"
